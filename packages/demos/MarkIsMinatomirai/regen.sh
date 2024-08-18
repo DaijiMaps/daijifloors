c7996a09836b53158c868cbb95b061f465e2b5bc
Masao Uebayashi <uebayasi@tombiinc.com>
2024-08-18 21:34:01 +0900

    regen.sh: copy more files

--- packages/demos/MarkIsMinatomirai/regen.sh
+++ packages/demos/MarkIsMinatomirai/regen.sh
@@ -1,14 +1,33 @@
 extract_cmd=${PWD}/../../cli/packages/map-extract-floors/dist/map-extract-floors.js
 
+mkdir -p data
+
 pushd data
 ${extract_cmd} ../floors.svg
 popd
 
 files='
-locs.json
+addresses/addresses_1F.json
+addresses/addresses_2F.json
+addresses/addresses_3F.json
+addresses/addresses_4F.json
+addresses/addresses_5F.json
+addresses/addresses_B1.json
+addresses/addresses_B2.json
+addresses/addresses_B4.json
+addresses/addresses_GL.json
+facilities.json
+floors.json
+header.ts
 locs-wide.json
+locs.json
+logo.svg
+params.ts
+shops.json
 '
 
-cp ${files} data
+for f in ${files}; do
+  cp ${f} data/${f}
+done
 
 pnpx prettier -w data
