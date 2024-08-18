b8748f63c1b7474b29a7beca7fde50d6db474bdf
Masao Uebayashi <uebayasi@tombiinc.com>
2024-08-18 16:00:24 +0900

    import MarkIsMinatomirai files

---
+++ packages/demos/MarkIsMinatomirai/shops.sh
@@ -0,0 +1,2 @@
+#/Applications/LibreOffice.app/Contents/MacOS/soffice --headless --convert-to csv:"Text - txt - csv (StarCalc)":"59,ANSI,1" shops.fods
+/Applications/LibreOffice.app/Contents/MacOS/soffice --headless --convert-to csv:"Text - txt - csv (StarCalc)" shops.fods
